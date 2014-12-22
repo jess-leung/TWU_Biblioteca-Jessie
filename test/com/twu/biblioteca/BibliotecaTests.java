c34b63a0e76ff9ba2ffaa0cd4efa3b054c64bb10
Jessie Leung <jleu1656@gmail.com>
2014-12-23 10:37:20 +1100

    First test - checking a book's details

--- test/com/twu/biblioteca/BibliotecaTests.java
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -6,6 +6,15 @@ import static org.junit.Assert.assertEquals;
 
 public class BibliotecaTests {
 
+    Library library;
+
+    public void setUp(){
+        library = new Library();
+        library.addBook("Harry Potter and the Philosopher's Stone","JK Rowling","1997");
+        library.addBook("World War Z","Max Brooks","2006");
+        library.addBook("Artificial Intelligence","Peter Norvig and Stuart J. Russell","1994");
+    }
+
     @Test
     public void testListBooks(){
 
@@ -13,7 +22,12 @@ public class BibliotecaTests {
 
     @Test
     public void testBookDetails(){
-
+        setUp();
+        Book chosenBook = Library.getBook(1);
+        assertEquals("World War Z",chosenBook.getTitle());
+        assertEquals("Max Brooks",chosenBook.getAuthor());
+        assertEquals("2006",chosenBook.getYear());
+        assertEquals("Available",chosenBook.getStatus());
     }
 
     @Test
