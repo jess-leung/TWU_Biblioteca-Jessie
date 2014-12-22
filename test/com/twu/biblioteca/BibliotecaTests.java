d28b88c4eb585d572c57988e2105f81ae7912dd9
Jessie Leung <jleu1656@gmail.com>
2014-12-23 10:16:19 +1100

    Listing test cases

---
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -0,0 +1,43 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaTests {
+
+    @Test
+    public void testListBooks(){
+
+    }
+
+    @Test
+    public void testBookDetails(){
+
+    }
+
+    @Test
+    public void testInvalidMenuOption(){
+
+    }
+
+    @Test
+    public void testCheckoutBook(){
+
+    }
+
+    @Test
+    public void testUnsuccessfulCheckout(){
+
+    }
+
+    @Test
+    public void testReturnBook(){
+
+    }
+
+    @Test
+    public void testUnsuccessfulReturn(){
+
+    }
+}
