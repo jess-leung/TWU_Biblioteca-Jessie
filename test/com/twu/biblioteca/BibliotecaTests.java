14d6378fb1bc749f9c06e79e6a807497543ed914
Jessie Leung <jleu1656@gmail.com>
2014-12-23 11:18:11 +1100

    Adding a new test case for the listing of library books

--- test/com/twu/biblioteca/BibliotecaTests.java
+++ test/com/twu/biblioteca/BibliotecaTests.java
@@ -3,6 +3,9 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.*;
 
 public class BibliotecaTests {
 
@@ -17,7 +20,12 @@ public class BibliotecaTests {
 
     @Test
     public void testListBooks(){
-
+        setUp();
+        ArrayList<Book> correctList = new ArrayList<Book>();
+        correctList.add(new Book("Harry Potter and the Philosopher's Stone","JK Rowling","1997"));
+        correctList.add(new Book("World War Z","Max Brooks","2006"));
+        correctList.add(new Book("Artificial Intelligence","Peter Norvig and Stuart J. Russell","1994"));
+        assertTrue(correctList.equals(library.getListing()));
     }
 
     @Test
