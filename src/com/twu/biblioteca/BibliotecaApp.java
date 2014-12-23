9a254e99fd2bf3ca83d828b1448c0a93a364e33e
Jessie Leung <jleu1656@gmail.com>
2014-12-23 11:11:53 +1100

    Additions to getBook

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,11 +1,10 @@
 package com.twu.biblioteca;
 
+import java.util.*;
+
 public class BibliotecaApp {
 
-
-    public static void main(String[] args) {
-        BibliotecaApp library = new BibliotecaApp();
-    }
+    ArrayList<Book> library = new ArrayList<Book>();
 
     /**
      * Method to add a book to the library
@@ -15,13 +14,25 @@ public class BibliotecaApp {
         newBook.author = "Max Brooks";
         newBook.year = "2006";
         newBook.title = "World War Z";
+        library.add(newBook);
     }
 
     /**
      * Get a book's details from the library
      */
     public Book getBook(int idx){
-        Book thisNewBook = new Book();
-        return thisNewBook;
+        try{
+            Book thisBook = library.get(idx);
+            return thisBook;
+        }
+        catch(IndexOutOfBoundsException ex){
+            return null;
+        }
     }
+
+    public static void main(String[] args) {
+        BibliotecaApp library = new BibliotecaApp();
+    }
+
+
 }
